36ea8b2fc6a7433a4be72de19ef0aa951ace01e1
Mirzabahrom <tmirzabahrom@gmail.com>
2023-11-03 19:11:26 +0300

    first comit, added index.html and script.js

---
+++ script.js
@@ -0,0 +1,320 @@
+//console.log ('HELLO WORLD')
+
+/*const parol = 'pasworfnkjgnbkjd'
+ if (parol.length == 8) {
+    console.log('Sizning parolingiz vawe zorakan')
+ }else if (parol.length == 7){
+    console.log('Sizi paroliz botu uka ')    
+ }else{
+    console.log('sizi paroliz yoq')
+ }*/
+
+
+ /*const ismlar = ['Abdullah', 'Bahrom', 'Azam']
+ let ism = []
+ 
+ for (let i = 0; i < ismlar.length; i++) { 
+   
+   ism.push(ismlar[i].toUpperCase())
+ }
+
+console.log(ism)*/
+
+
+/*const ismlar = 'Abdullah, Bahrom, Azam'
+
+let ism = ismlar.split('')
+
+for (let i = 0; i < ism.length; i++){
+   console.log(ism[i]) 
+}*/
+
+//let ism = 'ismizi kiriting ukam '
+
+
+//
+// 50 dan kop 89 dan oz bosa orta holmiz
+// uyam buyam bomasa kambagalmiz
+
+/*const pul = ''
+if (pul >= 90 ){
+   console.log('men boyman ')
+}else if (pul > 50){
+   console.log('men orta holman')
+}else{
+   console.log('men kambagalman')
+}*/
+
+/*const kod = 'miso'
+if (kod.length >= 12 && kod.includes('@')) {
+   console.log('sizning misolingiz juda togri')
+}else if (kod.length >= 7 || (kod.includes('@') && kod.length >= 6)) {
+   console.log('sizning misollingiz ortacha ')
+ } else { 
+   console.log('siz umuman notogri qildiz')
+ }*/
+
+//let user = false
+//if (!user){
+  // console.log('siz itam yoq')
+//}
+
+
+
+
+
+
+
+
+
+
+
+// promt, alert, let2, charAt(), slice(),toup(),tolowe()
+
+//let ism = prompt('ewufiewhuigewui') 
+                
+              
+//let ism2 = ism.charAt().toUpperCase() + ism.slice(1).toLowerCase() 
+//alert (ism2 )
+                                                                                                                                                                                                                                                                
+
+// prompt, 2alert, if, else, let, ``
+
+/*let men = prompt('siz qaysi viloyatan')
+if (men =='andijon'){
+   alert(`siz shu shahardansz ${men}`)
+}else 
+alert(`siz buyerdanmasz ${men}`)*/
+
+
+
+//let yosh = prompt('yoshizi kiriting')
+//alert(`siz shu yoshdasiz ${yosh}`)
+
+
+
+/*let ball = 56
+if (ball >= 90 ){
+   console.log(`siz ${ball} ball bilan imtihondan otingiz `.toUpperCase())
+}else if (ball >= 70){
+   console.log('Siz ortacha ball olip otingiz '.toUpperCase())
+}else {
+   console.log('siz umuman pas baho oldiz')*/
+
+   
+ //let email = 'tmirzabahrom@gmail.com'
+ /*let email1 = email.indexOf('a')
+ console.log(email1)  
+ let email2 = email.lastIndexOf('a')
+ console.log(email2)  
+   
+ let email3 = email.slice(1)
+ console.log(email3)*/  
+   
+/*let email4 = email.substr(0,12).length   
+   
+ console.log(email4) */ 
+   
+/*let email5 = email.replace('t','m')
+console.log(email5)*/
+
+//let email6 = email.charAt().toUpperCase()
+//let email7 = email.slice(1)
+//let email8 = email
+
+//console.log(email6 +email7)
+
+//let email9 = email.trim()
+
+//console.log(email9.length,email.length)
+
+/*let email10 = email.split('')
+console.log(email10)*/
+
+//let radius = 10
+//const pi = 3.14 
+
+//console.log(radius,pi)
+
+//math operators +,-,*,/,**,%
+
+//console.log(20 / 5)
+
+//let misol = 102 % 100
+//console.log(misol)
+
+//let  misol =pi * radius  ** 2
+
+//console.log(misol)
+
+//let misol = 5 * (10 - 8 ) ** 2
+//console.log(misol)
+
+//let layk = 20 
+//layk =layk +1
+//layk ++
+//layk--
+//layk = layk +10
+//layk +=12
+//layk -=10
+//layk /=2
+//layk *=10
+//let karam = 'karamimiz hozirda '.toUpperCase() + layk +'ta laykga ega'.toUpperCase()
+//console.log(karam)
+
+// template string
+/*const panda = 'pandaning otasini lagmon honasi qachon ochiladi   ??'
+const shinfu = 'pandaning choyhonasida '
+const ugve = 3500000
+//let ajdar = 'bu joy nomi' +' ' + panda + ' ' + shinfu +' ' + ugve + 'ta layk bor'
+//console.log(ajdar)
+
+let kalit = `Bu joy nomi ${panda} ${shinfu} ${ugve} ta layk bor `
+
+console.log(kalit)*/
+
+/*const iqbol = 'iqbolda 6 ta dom bor '
+const odil = 'odilda 7 ta dom bor '
+const jalon = 'jalonda 8 ta dom bor '
+let vadim = `vadim kimda nechta dom bor dep soradi men aytim ${iqbol} va ${odil} va ${jalon} hozircha shular shu aytilgan domlarga qarashyapti `.toUpperCase()
+console.log(vadim)*/
+
+//const mehmonlar = ['iqbol', 'odil' , 'jalon', 'vadim']
+ //mehmonlar[1]= 'akbar'
+ //console.log(mehmonlar)
+
+//let number = [1,2,3,4,5,6,7,123]
+
+//property
+//const kalit = number.length
+
+//console.log(kalit)
+
+//let kalit = mehmonlar. concat(number)
+//mehmonlar.push('soyipjon') = mehmonlar.pop('iqbol')
+//mehmonlar.pop('vadim')
+//console.log(mehmonlar)
+
+// Null Undefined
+ 
+// booleans & comparisons
+//console.log(true,false, 'true','false')
+
+// methods can return booleans
+//let email = 'tmirzabahrom@gmail.com'
+
+//let kalit = email.includes('@')
+//console.log(kalit)
+
+//comparisons operators
+
+//let age = 34
+//console.log(age >= 35)
+//console.log(age != 39)
+//console.log(age >+30)
+//console.log(age <= 44)
+//console.log(age ==34)
+//console.log(age >= 20)
+
+//let ism = 'mirza'
+
+/*console.log(ism >= 'mirza')
+console.log(ism >='mirza')
+console.log(ism >= 'Mirza')*/
+
+//let yosh = 25
+
+//console.log(yosh == 25)
+//console.log(yosh == '25')
+
+/*console.log(yosh != 25)
+console.log(yosh != '25')
+console.log(yosh !=24)*/
+
+//console.log(yosh ===25)
+//console.log(yosh === '25')
+
+//let natija = '100'
+//console.log(typeof natija)
+//natija = Number(natija)
+//console.log(natija + 1)
+//let hisob = String(15)
+//console.log(typeof hisob)
+
+//let kalit = natija 
+//console.log( Number(natija) )
+
+//let kalit = '2003
+//console.log(typeof (kalit))
+
+
+ /*let ism = prompt('ismingizni kiriting')
+ let ism1 = ism.charAt().toUpperCase()
+ let ism2 = ism.slice(1).toLowerCase()
+ let ism3 = ism1 + ism2
+
+ 
+ alert(`salom, ${ism3}`)*/
+
+/* kalit = prompt('ismini kirit')
+let kalit1 = kalit.charAt().toUpperCase()
+let kalit2 = kalit.slice(1).toLowerCase()
+let kalit3 = kalit1 + kalit2
+alert(`salom,${kalit3}`)*/
+
+
+// for loop
+for (let i = 0; 0 < 10; i++){
+
+   console.log('mirzabahrom')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
